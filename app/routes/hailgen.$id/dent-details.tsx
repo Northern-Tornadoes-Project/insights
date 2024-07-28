fdfa15dbc5c5a9dc7788607011a0668d5e4c300d
Kevin Manka <113623001+kevin-manka@users.noreply.github.com>
2024-07-28 13:35:52 -0400

    feat: dent index input control

--- app/routes/hailgen.$id/dent-details.tsx
+++ app/routes/hailgen.$id/dent-details.tsx
@@ -1,56 +1,126 @@
+import { CornerDownLeft, Pencil, Plus, Trash2 } from 'lucide-react';
 import { useEffect, useState } from 'react';
+import { Button } from '~/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/components/ui/card';
+import { Input } from '~/components/ui/input';
+import { Popover, PopoverContent, PopoverTrigger } from '~/components/ui/popover';
 
 interface HailpadDent {
-	// TODO: Use shared interface
-	angle: string | null;
-	centroidX: string;
-	centroidY: string;
-	majorAxis: string;
-	minorAxis: string;
+    // TODO: Use shared interface
+    angle: string | null;
+    centroidX: string;
+    centroidY: string;
+    majorAxis: string;
+    minorAxis: string;
 }
 
-function Detail({ label, value }: { label: string; value: string }) {
-	return (
-		<div className="flex flex-col">
-			<p className="text-sm text-muted-foreground">{label}</p>
-			<p>{value}</p>
-		</div>
-	);
+function Detail({ label, value }: { label: string; value?: string }) {
+    return (
+        <div className="flex flex-col">
+            <p className="text-sm text-muted-foreground">{label}</p>
+            <p>{value}</p>
+        </div>
+    );
 }
 
 export default function DentDetails({
-	dentData,
-	index,
-	onPrevious,
-	onNext,
-	onIndexChange
+    dentData,
+    index,
+    onPrevious,
+    onNext,
+    onIndexChange
 }: {
-	dentData: HailpadDent[];
-	index: number;
-	onPrevious?: () => void;
-	onNext?: () => void;
-	onIndexChange: (index: number) => void;
+    dentData: HailpadDent[];
+    index: number;
+    onPrevious?: () => void;
+    onNext?: () => void;
+    onIndexChange: (index: number) => void;
 }) {
-	const [minor, setMinor] = useState<number>(0);
-	const [major, setMajor] = useState<number>(0);
+    const [minor, setMinor] = useState<number>(0);
+    const [major, setMajor] = useState<number>(0);
 
-	useEffect(() => {
-		if (dentData.length === 0) return;
-		setMinor(Number(dentData[index].minorAxis));
-		setMajor(Number(dentData[index].majorAxis));
-	}, [dentData, index]);
+    const [currentIndex, setCurrentIndex] = useState<number | null>(null);
 
-	return (
-		<Card>
-			<CardHeader>
-				<CardTitle>Dent Details</CardTitle>
-				<CardDescription>About the selected dent.</CardDescription>
-			</CardHeader>
-			<CardContent className="mt-4 grid grid-cols-3 gap-4">
-				<Detail label="Minor Axis" value={`${minor.toFixed(2)} mm`} />
-				<Detail label="Major Axis" value={`${major.toFixed(2)} mm`} />
-			</CardContent>
-		</Card>
-	);
+    useEffect(() => {
+        if (dentData.length === 0) return;
+        setMinor(Number(dentData[index].minorAxis));
+        setMajor(Number(dentData[index].majorAxis));
+    }, [dentData, index]);
+
+    return (
+        <Card>
+            <CardHeader>
+                <div className="flex flex-row justify-between">
+                    <div>
+                        <CardTitle className="mb-2">Dent Details</CardTitle>
+                        <CardDescription>About the selected dent.</CardDescription>
+                    </div>
+                    <div className="justify-end space-x-2">
+                        <Popover>
+                            <PopoverTrigger>
+                                <Button asChild variant="outline" className="w-8 h-8 p-2">
+                                    <Trash2 />
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent>
+                                TODO
+                            </PopoverContent>
+                        </Popover>
+                        <Popover>
+                            <PopoverTrigger>
+                                <Button asChild variant="outline" className="w-8 h-8 p-2">
+                                    <Pencil />
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent>
+                                TODO
+                            </PopoverContent>
+                        </Popover>
+                        <Popover>
+                            <PopoverTrigger>
+                                <Button asChild variant="outline" className="w-8 h-8 p-2">
+                                    <Plus />
+                                </Button>
+                            </PopoverTrigger>
+                            <PopoverContent>
+                                TODO
+                            </PopoverContent>
+                        </Popover>
+                    </div>
+                </div>
+            </CardHeader>
+            <CardContent>
+                <div className="col-span-2">
+                    <Detail label="Dent" />
+                    <div className="flex flex-row items-center mt-1 gap-2">
+                        <Input
+                            className="w-20 h-8"
+                            type="number"
+                            min={0}
+                            max={dentData.length + 1}
+                            placeholder={`${index + 1}`}
+                            onChange={(e) => setCurrentIndex(parseInt(e.target.value))}
+                        />
+                        <p>/</p>
+                        <p>{`${dentData.length + 1}`}</p>
+                        <Button
+                            className="w-8 h-8 p-2 ml-2"
+                            variant="secondary"
+                            size="icon"
+                            onClick={() => {
+                                onIndexChange(Number(currentIndex) - 1);
+                                setCurrentIndex(null);
+                            }}
+                        >
+                            <CornerDownLeft />
+                        </Button>
+                    </div>
+                </div>
+                <div className="grid grid-cols-3 mt-4 gap-4">
+                    <Detail label="Minor Axis" value={`${minor.toFixed(2)} mm`} />
+                    <Detail label="Major Axis" value={`${major.toFixed(2)} mm`} />
+                </div>
+            </CardContent>
+        </Card>
+    );
 }

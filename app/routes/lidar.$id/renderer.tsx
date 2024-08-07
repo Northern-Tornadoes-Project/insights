576b6f9927089c2f2d956821c63c07a4d03bebde
Collin Town <35181633+towner-10@users.noreply.github.com>
2024-08-07 19:25:18 -0400

    fix: correct colour mapping in Potree

--- app/routes/lidar.$id/renderer.tsx
+++ app/routes/lidar.$id/renderer.tsx
@@ -1,6 +1,6 @@
 import { FlyControls, PointerLockControls } from '@react-three/drei';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Potree, type PointCloudOctree } from 'potree-core';
+import { PointSizeType, Potree, type PointCloudOctree } from 'potree-core';
 import { useEffect, useState } from 'react';
 import { cn } from '~/lib/utils';
 import { useStore } from './store';
@@ -9,33 +9,46 @@ const potree = new Potree();
 
 function Renderer() {
 	const { size, shape, budget, setCameraPosition, setCameraRotation } = useStore();
-	const { scene } = useThree();
+	const scene = useThree((state) => state.scene);
 	const [pointClouds, setPointClouds] = useState<PointCloudOctree[]>([]);
 
 	useEffect(() => {
-		const load = async () => {
+		potree.pointBudget = budget;
+
+		(async () => {
 			const result = await potree.loadPointCloud(
 				'metadata.json',
 				(url) => `/scans/aceb_scan/output/${url}`
 			);
 
 			// Ensure the axes are aligned with the world axes
+			result.position.set(0, 0, 0);
 			result.rotation.x = -Math.PI / 2;
-			result.material.pointSizeType = 0;
+			result.material.shape = shape;
+			result.material.size = size;
+
+			result.material.inputColorEncoding = 1;
+			result.material.outputColorEncoding = 1;
+
+			// Set the point size type to adaptive
+			result.material.pointSizeType = PointSizeType.ADAPTIVE;
+
+			// Set to proper shader
+			result.material.updateShaderSource();
+
+			console.log(result.material);
 
 			scene.clear();
 			scene.add(result);
 			setPointClouds([result]);
-		};
-
-		potree.pointBudget = budget;
-
-		load();
+		})();
 	}, []);
 
 	useEffect(() => {
 		potree.pointBudget = budget;
+	}, [budget]);
 
+	useEffect(() => {
 		if (!pointClouds.length) return;
 
 		// Only ever one point cloud
@@ -43,10 +56,11 @@ function Renderer() {
 
 		pointCloud.material.shape = shape;
 		pointCloud.material.size = size;
-	}, [size, shape, budget]);
+	}, [size, shape]);
 
 	useFrame(({ gl, camera }) => {
 		potree.updatePointClouds(pointClouds, camera, gl);
+		gl.clear();
 		gl.render(scene, camera);
 		setCameraPosition([camera.position.x, camera.position.y, camera.position.z]);
 		setCameraRotation([camera.rotation.x, camera.rotation.y, camera.rotation.z]);
@@ -63,7 +77,7 @@ function Renderer() {
 
 export default function ({ className }: { className?: string }) {
 	return (
-		<div className={cn('relative rounded-lg border bg-card shadow-sm h-full w-full', className)}>
+		<div className={cn('relative h-full w-full rounded-lg border bg-card shadow-sm', className)}>
 			<p className="absolute bottom-3 left-5 z-10 text-2xl">
 				<span className="font-bold">NTP</span> LiDAR
 			</p>
